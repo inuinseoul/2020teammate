6d7297d95d2a53d81ec5d7a2cf0b87ddb338f621
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2020-09-16 12:06:26 +0900

    합산 컬럼 추가
    합산 컬럼 추가

--- 인공지능 서비스 프로젝트/ai_teammate/users/models.py
+++ 인공지능 서비스 프로젝트/ai_teammate/users/models.py
@@ -2,77 +2,84 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-#유저 테이블
+# 유저 테이블
 class Customer(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='customer')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer")
     name = models.CharField(max_length=10)
     email = models.CharField(max_length=20, null=True)
     phone_num = models.CharField(max_length=20, null=True)
-    liked_users = models.ManyToManyField(User,related_name='liked_users')
+    liked_users = models.ManyToManyField(User, related_name="liked_users")
 
-#흥미 테이블
+
+# 흥미 테이블
 class Domain(models.Model):
     foreignkey = models.ForeignKey(
-        Customer, related_name='domain', on_delete=models.CASCADE, null=True
+        Customer, related_name="domain", on_delete=models.CASCADE, null=True
     )
     health = models.IntegerField(default=0)
-    economy = models.IntegerField()
-    culture_art = models.IntegerField()
-    education = models.IntegerField()
-    society = models.IntegerField()
-    technology = models.IntegerField()
+    economy = models.IntegerField(default=0)
+    culture_art = models.IntegerField(default=0)
+    education = models.IntegerField(default=0)
+    society = models.IntegerField(default=0)
+    technology = models.IntegerField(default=0)
+    domain_sum = models.IntegerField(default=0)
 
-#프로필(점수) 테이블
+
+# 프로필(점수) 테이블
 class Score(models.Model):
     foreignkey = models.ForeignKey(
-        Customer, related_name='score', on_delete=models.CASCADE, null=True
+        Customer, related_name="score", on_delete=models.CASCADE, null=True
     )
-    web = models.IntegerField()
-    design = models.IntegerField()
-    machine_learning = models.IntegerField() 
-    statistics = models.IntegerField()
-    deep_learning = models.IntegerField()
-    algorithm = models.IntegerField()
-    nlp = models.IntegerField()
-    data_score = models.IntegerField()
-    modeling_score = models.IntegerField()
+    web = models.IntegerField(default=0)
+    design = models.IntegerField(default=0)
+    machine_learning = models.IntegerField(default=0)
+    statistics = models.IntegerField(default=0)
+    deep_learning = models.IntegerField(default=0)
+    algorithm = models.IntegerField(default=0)
+    nlp = models.IntegerField(default=0)
+    data_score = models.IntegerField(default=0)
+    modeling_score = models.IntegerField(default=0)
+    score_sum = models.IntegerField(default=0)
 
 
-#선호역할 테이블
+# 선호역할 테이블
 class Role(models.Model):
     foreignkey = models.ForeignKey(
-        Customer, related_name='role', on_delete=models.CASCADE, null=True
+        Customer, related_name="role", on_delete=models.CASCADE, null=True
     )
-    analysis_hearts = models.IntegerField()
-    web_hearts = models.IntegerField()
-    design_hearts = models.IntegerField()
-    modeling_hearts = models.IntegerField()
+    analysis_hearts = models.IntegerField(default=0)
+    web_hearts = models.IntegerField(default=0)
+    design_hearts = models.IntegerField(default=0)
+    modeling_hearts = models.IntegerField(default=0)
+    role_sum = models.IntegerField(default=0)
 
-#선호스터디 테이블
+
+# 선호스터디 테이블
 class Study(models.Model):
     foreignkey = models.ForeignKey(
-        Customer, related_name='study', on_delete=models.CASCADE, null=True
+        Customer, related_name="study", on_delete=models.CASCADE, null=True
     )
-    web_hearts = models.IntegerField()
-    design_hearts = models.IntegerField()
-    machine_learning_hearts = models.IntegerField() 
-    statistics_hearts = models.IntegerField()
-    deep_learning_hearts = models.IntegerField()
-    algorithm_hearts = models.IntegerField()
-    nlp_hearts = models.IntegerField()
-    basic_python_hearts = models.IntegerField()
-    data_analysis_hearts = models.IntegerField()
-    voice_recog_hearts = models.IntegerField()
-    computer_vision_hearts = models.IntegerField()
-    rec_system_hearts = models.IntegerField()
-    reinforcement_hearts = models.IntegerField()
+    web_hearts = models.IntegerField(default=0)
+    design_hearts = models.IntegerField(default=0)
+    machine_learning_hearts = models.IntegerField(default=0)
+    statistics_hearts = models.IntegerField(default=0)
+    deep_learning_hearts = models.IntegerField(default=0)
+    algorithm_hearts = models.IntegerField(default=0)
+    nlp_hearts = models.IntegerField(default=0)
+    basic_python_hearts = models.IntegerField(default=0)
+    data_analysis_hearts = models.IntegerField(default=0)
+    voice_recog_hearts = models.IntegerField(default=0)
+    computer_vision_hearts = models.IntegerField(default=0)
+    rec_system_hearts = models.IntegerField(default=0)
+    reinforcement_hearts = models.IntegerField(default=0)
+    study_sum = models.IntegerField(default=0)
 
-#알리미
+
+# 알리미
 class Message(models.Model):
-    sender = models.CharField(max_length=20) #보낸사람
-    recipient = models.CharField(max_length=20) #받을사람
-    sentAt = models.DateTimeField(auto_now_add=True) #쪽지를보낸시간
+    sender = models.CharField(max_length=20)  # 보낸사람
+    recipient = models.CharField(max_length=20)  # 받을사람
+    sentAt = models.DateTimeField(auto_now_add=True)  # 쪽지를보낸시간
 
     def save(self, **kwargs):
         if not self.id:
