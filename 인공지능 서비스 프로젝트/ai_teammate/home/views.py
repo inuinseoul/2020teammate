95f6db6d82f1511d54a7e6a2e0b4e76be8ead856
LHS <gudtjr121@naver.com>
2020-09-19 15:13:20 +0900

    알림 수정

--- 인공지능 서비스 프로젝트/ai_teammate/home/views.py
+++ 인공지능 서비스 프로젝트/ai_teammate/home/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from users.models import Message, Study_Message
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    message_list = Message.objects.all()
+    study_message_list = Study_Message.objects.all()
+    
+    message_pk =[]
+    for i in message_list:
+        if i.recipient_pk not in message_pk:
+            message_pk.append(i.recipient_pk)
+
+    study_pk = []
+    for j in study_message_list:
+        if j.recipient_pk not in study_pk:
+            study_pk.append(j.recipient_pk) 
+
+    context = {'message_pk':message_pk, 'study_pk':study_pk}
+
+    return render(request, 'home/home.html',context)
+    
