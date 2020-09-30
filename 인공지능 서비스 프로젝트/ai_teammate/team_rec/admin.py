7f87463c99a7ef7894f858babb7ded0a127f5b67
DESKTOP-NI8SJKI\leeso <leeso226@naver.com>
2020-09-30 20:36:27 +0900

    팀 테이블, 팀원 테이블 만들기 --> makemigrations team_rec 해야함

--- 인공지능 서비스 프로젝트/ai_teammate/team_rec/admin.py
+++ 인공지능 서비스 프로젝트/ai_teammate/team_rec/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Team_make, Team_member
 # Register your models here.
+admin.site.register(Team_make)
+admin.site.register(Team_member)
--- 인공지능 서비스 프로젝트/ai_teammate/team_rec/models.py
+++ 인공지능 서비스 프로젝트/ai_teammate/team_rec/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+# from users.models import Customer
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+
+class Team_make(models.Model) :
+    leader = models.ForeignKey(
+        User, related_name="leader", on_delete=models.CASCADE, null=True
+    ) # 팀장
+    name = models.CharField(max_length=20) # 팀명
+    intro = models.TextField() # 팀 소개
+    state = models.IntegerField(default=0) # 팀상태
+    
+
+class Team_member(models.Model) : 
+    team_id = models.ForeignKey(
+        Team_make, related_name="team_id", on_delete=models.CASCADE, null=True
+    )
+    member = models.ForeignKey(
+    User, related_name="team_member", on_delete=models.CASCADE, null=True
+    )
