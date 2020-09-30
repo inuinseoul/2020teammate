683bb24c70428135934ca643a0141e86018ef04b
DESKTOP-NI8SJKI\leeso <leeso226@naver.com>
2020-09-30 17:50:38 +0900

    team_rec_list --> person 으로 URL 이름 변경

--- 인공지능 서비스 프로젝트/ai_teammate/team_rec/urls.py
+++ 인공지능 서비스 프로젝트/ai_teammate/team_rec/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = "team_rec"
 urlpatterns = [
-    path("team_rec_list/<int:customer_pk>", views.team_rec_list, name="team_rec_list"),
+    # path("choice", views.choice, name="choice"),
+    # path("team_rec_list/<int:customer_pk>", views.team_rec_list, name="team_rec_list"),
+    path("person/<int:customer_pk>", views.team_rec_list, name="person"),
 ]
